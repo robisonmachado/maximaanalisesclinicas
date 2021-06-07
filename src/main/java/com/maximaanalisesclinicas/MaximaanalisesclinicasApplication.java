54375d6923e5302b5525c2d796d88f21bc3bde85
robisonmachado <robisonpmachado@gmail.com>
2021-06-07 18:09:51 -0300

    added initial spring boot project

---
+++ src/main/java/com/maximaanalisesclinicas/MaximaanalisesclinicasApplication.java
@@ -0,0 +1,13 @@
+package com.maximaanalisesclinicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximaanalisesclinicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaximaanalisesclinicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maximaanalisesclinicas/MaximaanalisesclinicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maximaanalisesclinicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaximaanalisesclinicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
